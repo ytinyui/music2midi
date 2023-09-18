68bc5522a235dd9978d5acb346b1432055fa21f9
ytinyui <ytinyui@gmail.com>
2023-09-18 23:07:55 +0800

    preprocess code

--- preprocess/filter_songs.py
+++ preprocess/filter_songs.py
@@ -6,12 +6,18 @@ import argparse
 from joblib import Parallel, delayed
 
 
-def filter_condition(yaml):
-    melody_chroma_accuracy = yaml["eval"]["melody_chroma_accuracy"]
-    song_dur = yaml["song"]["duration"]
-    piano_dur = yaml["piano"]["duration"]
-    dur_ratio = abs(song_dur - piano_dur) / song_dur
-    return melody_chroma_accuracy < 0.15 or dur_ratio > 0.2
+def filter_condition(yaml, condition):
+    if condition == "melody_chroma_accuracy":
+        melody_chroma_accuracy = yaml["eval"]["melody_chroma_accuracy"]
+        return melody_chroma_accuracy < 0.15
+
+    if condition == "duration":
+        song_dur = yaml["song"]["duration"]
+        piano_dur = yaml["piano"]["duration"]
+        dur_ratio = abs(song_dur - piano_dur) / song_dur
+        return dur_ratio > 0.2
+
+    raise ValueError(f"Unknown condition: {condition}")
 
 
 def run(file):
@@ -25,7 +31,13 @@ def run(file):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("path", type=str, default=None, help="provided audio")
+    parser.add_argument(
+        "--condition",
+        type=str,
+        default="melody_chroma_accuracy",
+        help="condition to filter, should be one of [melody_chroma_accuracy, duration]",
+    )
     args = parser.parse_args()
 
     file_paths = Path(args.path).glob("*.yaml")
-    Parallel(n_jobs=4)(delayed(run)(file) for file in file_paths)
+    Parallel(n_jobs=4)(delayed(run)(file, args.condition) for file in file_paths)
