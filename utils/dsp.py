81d2be0e66cba7e24c36ed9da5e806dea78384b2
ytinyui <ytinyui@gmail.com>
2023-10-15 12:15:55 +0800

    to_stereo()

--- utils/dsp.py
+++ utils/dsp.py
@@ -2,6 +2,22 @@ import numpy as np
 from scipy.interpolate import interp1d
 
 
+def pad_audio(x: np.ndarray, y: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
+    """
+    Pad the shorter audio to the longer one.
+    Returns a tuple of the two arrays.
+    """
+    return (
+        np.pad(x, (0, max(len(y) - len(x), 0))),
+        np.pad(y, (0, max(len(x) - len(y), 0))),
+    )
+
+
+def to_stereo(x, y):
+    x, y = pad_audio(x, y)
+    return np.stack((x, y))
+
+
 def normalize(audio, min_y=-1.0, max_y=1.0, eps=1e-8):
     assert len(audio.shape) == 1
     max_y -= eps
