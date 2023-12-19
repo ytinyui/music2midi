aed521ffc38c7cc5e75645baff5bc7cecb59aa8b
ytinyui <ytinyui@gmail.com>
2023-12-19 20:54:57 +0800

    multithread download midi

--- data/download_midi.py
+++ data/download_midi.py
@@ -2,12 +2,14 @@ import argparse
 import json
 import os
 import re
+import shutil
 import subprocess
 import time
 from pathlib import Path
 
 import requests
 from bs4 import BeautifulSoup
+from joblib import Parallel, delayed
 from omegaconf import DictConfig, OmegaConf
 from requests.adapters import HTTPAdapter
 from tqdm import tqdm
@@ -38,7 +40,7 @@ def get_session() -> requests.Session:
 
 def get_page_url(config: DictConfig, difficulty: str, genre: str, page_no: int):
     assert type(page_no) is int and page_no > 0
-    return "https://musescore.com/sheetmusic?sort=rating&instrument=2&instrumentation=114&complexity={}&genres={}&page={}".format(
+    return "https://musescore.com/sheetmusic?type=non-official&sort=viewcount&instrument=2&instrumentation=114&complexity={}&genres={}&page={}".format(
         config.code.difficulty[difficulty], config.code.genre[genre], page_no
     )
 
@@ -51,33 +53,54 @@ def get_content(url: str, session: requests.Session) -> dict:
         div = soup.find("div", {"class": re.compile("js-(?!page)")})
         key = next((k for k, v in div.attrs.items() if "data-" in k), None)
         content = json.loads(div.get(key))
-    except TypeError as e:
-        raise e(
-            f"Metadata not found for {url}. You are probably blocked by Cloudflare."
+    except (AttributeError, TypeError) as e:
+        print(
+            f"{e}. Metadata not found for {url}. You are probably blocked by Cloudflare."
         )
+
+        raise
     return content
 
 
 def get_midi_url_list(page_url: str, session: requests.Session) -> list[str]:
+    """
+    Return: list of urls on the webpage.
+    """
     content = get_content(page_url, session)
     scores = content["store"]["page"]["data"]["scores"]
     return [x["href"] for x in scores]
 
 
 def download_midi(
-    url: str, filename: str, output_dir: str, tmp_dir="tmp", max_retries: int = 5
+    url: str,
+    output_path: Path,
+    tmp_dir: Path,
+    max_retries: int = 5,
 ) -> int:
+    """
+    The program downloads the midi file to tmp_dir, then moves the file to the desired destination.
+
+    Return:
+    - 0 if the file is downloaded successfully
+    - 1 if the file is not downloaded
+    """
     retries = 0
     while True:
         subprocess.run(
-            ["python", "data/librescore.py", url, "--output_dir", tmp_dir],
+            [
+                "python",
+                "data/librescore.py",
+                url,
+                "--output_dir",
+                str(tmp_dir),
+            ],
             capture_output=True,
             text=True,
         )
-        file = next(Path(tmp_dir).glob("*.mid"), None)
+        file = next(tmp_dir.glob("*.mid"), None)
         if file is not None:
-            os.makedirs(output_dir, exist_ok=True)
-            file.rename(Path(output_dir) / filename)
+            os.makedirs(output_path.parent, exist_ok=True)
+            shutil.move(file, output_path)
             return 0
         else:
             retries += 1
@@ -85,11 +108,6 @@ def download_midi(
                 with open("midi_not_downloaded.txt", "a") as f:
                     f.write(url + "\n")
                 return 1
-            print(
-                "Midi file {} from {} not downloaded. Retrying... (attempt {}/{})".format(
-                    filename, url, retries, max_retries
-                )
-            )
 
 
 def get_metadata(url: str, session: requests.Session) -> dict:
@@ -105,6 +123,7 @@ def get_metadata(url: str, session: requests.Session) -> dict:
         "title": score_title,
         "duration": int(score_duration),
         "url": url,
+        "view_count": content["config"]["statistic"]["scores_views"],
     }
 
     song_data = content["store"]["page"]["data"]["song"]
@@ -118,107 +137,85 @@ def get_metadata(url: str, session: requests.Session) -> dict:
     return {"score": score_info, "song": song_info}
 
 
-def download_from_url_list(
-    url_list: list,
+def download_from_url(
+    url: str,
     session: requests.Session,
-    output_dir: str,
-    difficulty: str,
+    output_dir: Path,
+    difficulty: Path,
     genre: str,
     min_duration: int,
-    tmp_dir: str,
-) -> int:
-    """
-    return: number of files successfully downloaded
-    """
-    midi_dir = Path(output_dir) / "midi"
-    yaml_dir = Path(output_dir) / "metadata"
-    midi_dir.mkdir(exist_ok=True)
-    yaml_dir.mkdir(exist_ok=True)
+    tmp_dir: Path,
+) -> None:
+    midi_dir = output_dir / "midi"
+    yaml_dir = output_dir / "metadata"
+    os.makedirs(midi_dir, exist_ok=True)
+    os.makedirs(yaml_dir, exist_ok=True)
 
-    result = []
-    for url in url_list:
-        metadata = get_metadata(url, session)
-        score_meta = metadata["score"]
-        if score_meta["duration"] < min_duration:
-            continue
-        score_meta["difficulty"] = difficulty
-        score_meta["genre"] = genre
+    metadata = get_metadata(url, session)
+    score_meta = metadata["score"]
+    if score_meta["duration"] < min_duration:
+        return
+    score_meta["difficulty"] = difficulty
+    score_meta["genre"] = genre
 
-        output_no = download_midi(
-            url,
-            filename=score_meta["id"] + ".mid",
-            output_dir=midi_dir,
-            tmp_dir=tmp_dir,
-        )
-        result.append(output_no)
-        if output_no == 0:
-            yaml_file = (yaml_dir / score_meta["id"]).with_suffix(".yaml")
-            OmegaConf.save(metadata, yaml_file)
-
-    return result.count(0)
+    score_id = score_meta["id"]
+    yaml_file = (yaml_dir / score_id).with_suffix(".yaml")
+    if yaml_file.exists():
+        return
+    tmp_dir = tmp_dir / score_id
+    os.makedirs(tmp_dir, exist_ok=True)
+    output_code = download_midi(
+        url,
+        output_path=(midi_dir / score_id).with_suffix(".mid"),
+        tmp_dir=tmp_dir,
+    )
+    os.rmdir(tmp_dir)
+    if output_code == 0:
+        OmegaConf.save(metadata, yaml_file)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("output_dir", type=str, default=None)
-    parser.add_argument("--tmp_dir", type=str, default="tmp")
-    parser.add_argument("--resume_url", type=str, default=None)
+    parser.add_argument(
+        "--tmp_dir",
+        type=str,
+        default="tmp",
+        help="Temporary directory for downloading midi files",
+    )
     args = parser.parse_args()
 
     config = OmegaConf.load("data/config.yaml")
-    resumed = 0 if args.resume_url is not None else 1
     difficulty_genre_pairs = [
         (x, y) for x in config.code.difficulty.keys() for y in config.code.genre.keys()
     ]
     session = get_session()
 
-    for difficulty, genre in tqdm(difficulty_genre_pairs):
-        num_files_required = config["num scores"][difficulty][genre]
-        progress_bar = tqdm(total=num_files_required)
-        progress_bar.set_description(f"Downloading genre: {genre} ({difficulty})")
+    for difficulty, genre in (pbar := tqdm(difficulty_genre_pairs)):
+        pbar.set_description(f"Downloading {genre} ({difficulty})")
 
-        num_files = 0
-        page_no = 1
-        while num_files < num_files_required and page_no <= 100:
+        for page_no in tqdm(range(1, 101)):
             page_url = get_page_url(config, difficulty, genre, page_no)
-            # handle resuming if resume_url is provided
-            if resumed == 0 and page_url != args.resume_url:
-                page_no += 1
-                if page_no > 100:
-                    break
-                continue
-            if resumed == 0 and page_url == args.resume_url:
-                resumed = 1
-                print(f"\nProgress resumed at {page_url}")
 
             try:
                 url_list = get_midi_url_list(page_url, session)
-                download_from_url_list(
-                    url_list,
-                    session,
-                    output_dir=args.output_dir,
-                    difficulty=difficulty,
-                    genre=genre,
-                    min_duration=config["min duration"],
-                    tmp_dir=args.tmp_dir,
+                if len(url_list) == 0:  # max page num exceeded
+                    break
+                Parallel(n_jobs=len(url_list), backend="threading")(
+                    delayed(download_from_url)(
+                        url,
+                        session,
+                        output_dir=Path(args.output_dir),
+                        difficulty=difficulty,
+                        genre=genre,
+                        min_duration=config["min duration"],
+                        tmp_dir=Path(args.tmp_dir),
+                    )
+                    for url in url_list
                 )
-            except TimeoutError as e:
-                print(f"{e}, skipping {page_url}.")
             except Exception as e:
+                print(e)
                 print(
-                    f"{e}, Failed to download from {page_url}. You may resume by providing the url for --resume_url."
+                    f"Failed to access {page_url}. This is probably a connection error. The program will stop."
                 )
                 exit(1)
-
-            yaml_files = [
-                OmegaConf.load(file)
-                for file in Path(args.output_dir).glob("metadata/*.yaml")
-            ]
-            num_files = sum(
-                1
-                for config in yaml_files
-                if config.score["difficulty"] == difficulty
-                and config.score["genre"] == genre
-            )
-            progress_bar.update(num_files - progress_bar.n)
-            page_no += 1
