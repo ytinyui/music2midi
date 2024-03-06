705e345bc6d330bab1f251e0346835ce73e870fa
ytinyui <43673200+ytinyui@users.noreply.github.com>
2024-03-06 11:46:35 +0800

    new training code (tokenize time instead of beats)

--- dataset.py
+++ dataset.py
@@ -1,153 +1,168 @@
-import torch
-import librosa
-import pytorch_lightning as pl
-from omegaconf import OmegaConf, DictConfig
-from torch.utils.data import Dataset, DataLoader
-from sklearn.model_selection import train_test_split
+import os
 from pathlib import Path
-import numpy as np
-from layer.input import LogMelSpectrogram
-from midi_tokenizer import get_tokenizer
-from transformers import T5Config
-from torch.nn.utils.rnn import pad_sequence
+from typing import Literal, NamedTuple
 
+import librosa
+import numpy as np
+import pytorch_lightning as pl
+import torch
+from omegaconf import OmegaConf
+from torch.nn.utils.rnn import pad_sequence
+from torch.utils.data import DataLoader, Dataset
+from transformers import T5Config
+
+from midi_tokenizer import TokenizerFactory
 
 PAD = T5Config.from_pretrained("t5-small").pad_token_id
-assert PAD == 0
 
 
-class PopDataModule(pl.LightningDataModule):
+class MetadataDict:
+    def __init__(self, data_dir: Path, config_path: str):
+        super().__init__()
+        self.data_dir = data_dir
+        self.config = OmegaConf.load(config_path)
+        self.key_dict = {
+            "genre": {k: v for k, v in self.config.genre_id.items()},
+            "difficulty": {k: v for k, v in self.config.difficulty_id.items()},
+        }
+
+    def get(self, key: Literal["genre", "difficulty"], score_id: str):
+        """
+        Get the genre or difficulty id based on score_id
+        """
+        meta = OmegaConf.load(self.data_dir / "metadata" / (score_id + ".yaml"))
+        return self.key_dict[key][meta.score.get(key)]
+
+
+class InputDataTuple(NamedTuple):
+    x: torch.Tensor
+    y: torch.Tensor
+    genre_id: torch.LongTensor
+    difficulty_id: torch.LongTensor
+
+
+class MyDataModule(pl.LightningDataModule):
     def __init__(self, data_dir: str, config_path: str = "config.yaml"):
         super().__init__()
-        self.config = OmegaConf.load(config_path)
+        self.config_path = config_path
+        self.config = OmegaConf.load(self.config_path)
         self.data_dir = Path(data_dir)
-        self.song_ids = [path.stem for path in self.data_dir.glob("*.yaml")]
+        self.dataset_split_ids = np.load(
+            self.data_dir / "dataset_split.npz", allow_pickle=True
+        )
 
     def setup(self, stage=None):
-        train_ids, val_ids = train_test_split(self.song_ids, test_size=0.1)
-        self.train_set = PopDataset(
-            self.data_dir, train_ids, self.config, transform=True
+        self.train_set = MyDataset(
+            self.data_dir,
+            self.dataset_split_ids.get("train_id"),
+            self.config_path,
         )
-        self.val_set = PopDataset(self.data_dir, val_ids, self.config)
+        self.val_set = MyDataset(
+            self.data_dir,
+            self.dataset_split_ids.get("val_id"),
+            self.config_path,
+        )
+        self.test_set = MyDataset(
+            self.data_dir,
+            self.dataset_split_ids.get("test_id"),
+            self.config_path,
+        )
 
     def train_dataloader(self):
         return DataLoader(
             self.train_set,
             shuffle=True,
             collate_fn=collate_fn,
-            **self.config.dataloader
+            **self.config.dataloader,
         )
 
     def val_dataloader(self):
-        return DataLoader(self.val_set, collate_fn=collate_fn, **self.config.dataloader)
+        return DataLoader(
+            self.val_set,
+            shuffle=False,
+            collate_fn=collate_fn,
+            **self.config.dataloader,
+        )
+
+    def test_dataloader(self):
+        return DataLoader(
+            self.test_set,
+            shuffle=False,
+            collate_fn=collate_fn,
+            **self.config.dataloader,
+        )
 
 
-class PopDataset(Dataset):
+class MyDataset(Dataset):
     def __init__(
         self,
         data_dir: Path,
-        song_ids: list[str],
-        config: DictConfig,
-        transform: bool = False,
+        score_ids: list[str],
+        config_path: Path,
     ):
         super().__init__()
-
-        self.config = config
+        self.config = OmegaConf.load(config_path)
+        self.metadata_dict = MetadataDict(data_dir, config_path)
         self.data_dir = data_dir
-        self.transform = transform
-        self.spectrogram = LogMelSpectrogram()
-        self.tokenizer = get_tokenizer(self.config.tokenizer)
+        self.tokenizer = TokenizerFactory.create_tokenizer(self.config.tokenizer)
 
         self.audio_paths = [
-            (self.data_dir / song_id).glob("*.pitchshift.wav").__next__()
-            for song_id in song_ids
+            os.path.join(data_dir, "audio", score_id + ".wav") for score_id in score_ids
         ]
         self.midi_notes = [
-            np.load((self.data_dir / song_id).glob("*notes.npy").__next__())
-            for song_id in song_ids
+            np.load(os.path.join(data_dir, "midi_numpy", score_id + ".npy"))
+            for score_id in score_ids
         ]
-        self.beattimes = [
-            np.load((self.data_dir / song_id).glob("*beattime.npy").__next__())
-            for song_id in song_ids
+        self.genre_ids = [
+            self.metadata_dict.get("genre", score_id) for score_id in score_ids
         ]
-
-        composer_dict = {}
-        for k, v in config.composer_ids.items():
-            composer_dict[k] = v
-        self.composer_ids = [
-            get_composer_id(str(self.data_dir / song_id) + ".yaml", composer_dict)
-            for song_id in song_ids
+        self.difficulty_ids = [
+            self.metadata_dict.get("difficulty", score_id) for score_id in score_ids
         ]
 
-    def __getitem__(self, index) -> (torch.Tensor, torch.Tensor):
-        n_bars = self.config.dataset.n_bars
+    def __getitem__(self, index) -> InputDataTuple:
+        duration = self.config.dataset.segment_duration
+        genre_id = self.genre_ids[index]
+        difficulty_id = self.difficulty_ids[index]
 
-        composer_id = torch.LongTensor([self.composer_ids[index]])
-
-        beattimes = self.beattimes[index]
-        downbeat_indices = [i for i, x in enumerate(beattimes) if i % 4 == 0][:-n_bars]
-
-        start_idx = np.random.choice(downbeat_indices)
-        notes_segment = get_notes_segment(
-            self.midi_notes[index], start_idx, start_idx + n_bars * 8
+        audio_path = self.audio_paths[index]
+        audio_duration = librosa.get_duration(path=audio_path)
+        start_time = np.random.choice(
+            np.arange(0, audio_duration - duration, self.tokenizer.time_step)
         )
-
-        waveform = get_audio_segment(
+        waveform, sr = librosa.load(
             self.audio_paths[index],
-            self.config.dataset.sample_rate,
-            beattimes,
-            start_idx,
-            start_idx + n_bars * 4,
+            sr=self.config.dataset.sample_rate,
+            offset=start_time,
+            duration=duration,
         )
+        notes_segment = get_notes_segment(
+            self.midi_notes[index], start_time, start_time + duration
+        )
+        tokens = self.tokenizer(notes_segment, start_time=start_time, add_eos=True)
 
-        # if self.transform:
-        #     velocity_shift_val = np.random.binomial(10, p=0.5) - 5
-        #     notes_segment = note_velocity_shift(notes_segment, value=velocity_shift_val)
-
-        # x.shape = (length, feat_dim)
-        x = self.spectrogram(waveform).transpose(-1, -2)
-        tokens = self.tokenizer(notes_segment, offset_idx=start_idx, add_eos=True)
-        y = torch.from_numpy(tokens)
-
-        return x, y, composer_id
+        return InputDataTuple(
+            x=torch.from_numpy(waveform),
+            y=torch.from_numpy(tokens),
+            genre_id=torch.LongTensor([genre_id]),
+            difficulty_id=torch.LongTensor([difficulty_id]),
+        )
 
     def __len__(self):
         return len(self.audio_paths)
 
 
-def get_composer_id(file_path: str, composer_dict: dict) -> int:
-    return composer_dict[OmegaConf.load(file_path).piano.uploader]
-
-
-def get_audio_segment(
-    audio_path: Path,
-    sample_rate: int,
-    beattimes: np.ndarray,
-    start_idx: int,
-    end_idx: int,
-) -> torch.Tensor:
-    start_time, end_time = beattimes[start_idx], beattimes[end_idx]
-    waveform, sample_rate = librosa.load(
-        str(audio_path),
-        sr=sample_rate,
-        offset=start_time,
-        duration=end_time - start_time,
-    )
-    return torch.from_numpy(waveform)
-
-
-def get_notes_segment(notes: np.ndarray, start_idx: int, end_idx: int) -> np.ndarray:
-    return notes[(notes[:, 0] >= start_idx) & (notes[:, 0] < end_idx)]
-
-
-def note_velocity_shift(notes: np.ndarray, value: int) -> np.ndarray:
-    notes[:, 3] = np.minimum(np.maximum(notes[:, 3] + value, 1), 127)
-    return notes
+def get_notes_segment(
+    notes: np.ndarray, start_time: float, end_time: float
+) -> np.ndarray:
+    return notes[(notes[:, 0] >= start_time) & (notes[:, 0] < end_time)]
 
 
 def collate_fn(batch):
-    x_batch, y_batch, id_batch = zip(*batch)
+    x_batch, y_batch, genre_id_batch, difficulty_id_batch = zip(*batch)
     x_batch = pad_sequence(x_batch, batch_first=True, padding_value=PAD)
     y_batch = pad_sequence(y_batch, batch_first=True, padding_value=PAD)
-    id_batch = torch.stack(id_batch)
-    return x_batch, y_batch, id_batch
+    genre_id_batch = torch.stack(genre_id_batch)
+    difficulty_id_batch = torch.stack(difficulty_id_batch)
+
+    return InputDataTuple(x_batch, y_batch, genre_id_batch, difficulty_id_batch)
--- layer/input.py
+++ layer/input.py
@@ -4,10 +4,10 @@ import torchaudio
 
 
 class LogMelSpectrogram(nn.Module):
-    def __init__(self) -> None:
+    def __init__(self, sample_rate: int) -> None:
         super().__init__()
         self.melspectrogram = torchaudio.transforms.MelSpectrogram(
-            sample_rate=22050,
+            sample_rate=sample_rate,
             n_fft=4096,
             hop_length=1024,
             f_min=10.0,
@@ -18,25 +18,33 @@ class LogMelSpectrogram(nn.Module):
         # x : audio(batch, sample)
         # X : melspec (batch, freq, frame)
         with torch.no_grad():
-            with torch.cuda.amp.autocast(enabled=False):
-                X = self.melspectrogram(x)
-                X = X.clamp(min=1e-6).log()
+            X = self.melspectrogram(x)
+            X = X.clamp(min=1e-6).log()
 
         return X
 
 
 class MelConditioner(nn.Module):
-    def __init__(self, n_vocab, n_dim=512) -> None:
+    def __init__(self, n_genre: int, n_difficulty: int, n_dim: int = 512) -> None:
         super().__init__()
-        self.embedding = nn.Embedding(num_embeddings=n_vocab, embedding_dim=n_dim)
+        self.embedding_genre = nn.Embedding(num_embeddings=n_genre, embedding_dim=n_dim)
+        self.embedding_difficulty = nn.Embedding(
+            num_embeddings=n_difficulty, embedding_dim=n_dim
+        )
 
-    def forward(self, feature: torch.Tensor, index: torch.Tensor):
+    def forward(
+        self,
+        feature: torch.Tensor,
+        genre_index: torch.Tensor,
+        difficulty_index: torch.Tensor,
+    ):
         """
-        Concatenate composer embedding to feature
+        Concatenate embeddings to feature
 
         feature: (batch, L, n_dim)
         index : (batch, )
         """
         # (batch, 1, feature_dim)
-        composer_embedding = self.embedding(index)
-        return torch.cat([composer_embedding, feature], dim=1)
+        embedding_genre = self.embedding_genre(genre_index)
+        embedding_difficulty = self.embedding_difficulty(difficulty_index)
+        return torch.cat([embedding_genre, embedding_difficulty, feature], dim=1)
