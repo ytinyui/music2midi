78f0292d36de541da3f1c65d5fecc23347bc5dba
ytinyui <43673200+ytinyui@users.noreply.github.com>
2024-03-06 11:46:35 +0800

    note validation

--- data/midi_to_numpy.py
+++ data/midi_to_numpy.py
@@ -4,8 +4,36 @@ from pathlib import Path
 
 import numpy as np
 from joblib import Parallel, delayed
+from numba import njit
 from pretty_midi import PrettyMIDI
 from tqdm import tqdm
+
+
+@njit()
+def fix_invalid_offsets(numpy_notes: np.ndarray) -> np.ndarray:
+    """
+    offset of the previous note should not be after onset of the next note with the same pitch
+    """
+    for pitch in np.unique(numpy_notes[:, 2]):
+        index = numpy_notes[:, 2] == pitch
+        notes_tmp = numpy_notes[index]
+        length = len(notes_tmp)
+        for i in range(length):
+            for j in range(length):
+                if notes_tmp[i, 0] < notes_tmp[j, 0]:
+                    notes_tmp[i, 1] = min(notes_tmp[i, 1], notes_tmp[j, 0])
+        numpy_notes[index] = notes_tmp
+    return numpy_notes
+
+
+def remove_instant_notes(
+    numpy_notes: np.ndarray, threshold: float = 0.005
+) -> np.ndarray:
+    """
+    remove notes which the duration is less than threshold
+    """
+    duration = numpy_notes[:, 1] - numpy_notes[:, 0]
+    return numpy_notes[duration >= threshold]
 
 
 def midi_to_numpy(midi_data: PrettyMIDI) -> np.ndarray:
@@ -36,6 +64,8 @@ def main(midi_path: Path, output_dir: Path):
         return
     midi_data = PrettyMIDI(str(midi_path))
     numpy_notes = midi_to_numpy(midi_data)
+    numpy_notes = fix_invalid_offsets(numpy_notes)
+    numpy_notes = remove_instant_notes(numpy_notes)
     np.save(output_path, numpy_notes)
 
 
