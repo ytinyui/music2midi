600764309f2035d3e4d77995336d9f780546a5f9
ytinyui <43673200+ytinyui@users.noreply.github.com>
2024-03-13 23:10:55 +0800

    inference code

--- src/model.py
+++ src/model.py
@@ -3,7 +3,7 @@ import multiprocessing
 from contextlib import redirect_stdout
 from datetime import datetime
 from pathlib import Path
-from typing import NamedTuple, Optional, Union
+from typing import Optional, Union
 
 import librosa
 import more_itertools
@@ -48,7 +48,10 @@ class EncoderDecoderTransformer(nn.Module):
         for k, v in self.config.gpt2.items():
             setattr(self.decoder_config, k, v)
 
+        encoder_hidden_size = self.encoder_config.hidden_size
+        decoder_hidden_size = self.decoder_config.n_embd
         self.encoder = Swinv2Model(self.encoder_config)
+        self.proj = nn.Linear(encoder_hidden_size, decoder_hidden_size)
         self.decoder = GPT2LMHeadModel(self.decoder_config)
 
         self.tokenizer = MidiTokenizer(self.config)
@@ -64,10 +67,14 @@ class EncoderDecoderTransformer(nn.Module):
         self.mel_conditioner = MelConditioner(
             n_genre,
             n_difficulty,
-            n_dim=self.config.gpt2.n_embd,
+            n_dim=decoder_hidden_size,
         )
 
     def forward(self, inputs: ModelInputs, **kwargs) -> dict:
+        labels = self.tokenizer(inputs.notes_batch, padding="left")
+        max_length = self.decoder.config.n_positions
+        if labels.shape[-1] > max_length:
+            labels = labels[:, :max_length]
         if inputs.input_waveform is None:
             return self.decoder(input_ids=labels, labels=labels, **kwargs)
 
@@ -75,17 +82,17 @@ class EncoderDecoderTransformer(nn.Module):
         b, _, h, w = encoder_inputs.shape
         assert (
             h == w == self.encoder_config.image_size
-        ), f"n_mels({h}) does not match sequence length({w})"
+        ), f"(n_mels, sequence length) = {(h,w)} and encoder image size ({self.encoder_config.image_siz}) mismatch"
 
-        encoder_last_hidden_state = self.encoder(encoder_inputs).last_hidden_state
-        if inputs.genre_id is None and inputs.difficulty_id is None:
-            encoder_hidden_states = encoder_last_hidden_state
+        encoder_out = self.encoder(encoder_inputs)
+        hidden_states = self.proj(encoder_out.last_hidden_state)
+        if inputs.genre_id is None or inputs.difficulty_id is None:
+            hidden_states_conditioned = hidden_states
         else:
-            encoder_hidden_states = self.mel_conditioner(
-                encoder_last_hidden_state, inputs.genre_id, inputs.difficulty_id
+            hidden_states_conditioned = self.mel_conditioner(
+                hidden_states, inputs.genre_id, inputs.difficulty_id
             )
 
-        labels = self.tokenizer(inputs.notes_batch, padding="left")
         labels = labels.to(self.decoder.device)
         attention_mask = labels != self.decoder.config.pad_token_id
 
@@ -93,15 +100,15 @@ class EncoderDecoderTransformer(nn.Module):
             input_ids=labels,
             attention_mask=attention_mask,
             labels=labels,
-            encoder_hidden_states=encoder_hidden_states,
+            encoder_hidden_states=hidden_states_conditioned,
             **kwargs,
         )
 
         if inputs.notes_waveform is not None:
             notes_spectrogram = self.spectrogram(inputs.notes_waveform)
+            encoder_out2 = self.encoder(notes_spectrogram)
             ct_loss = self.contrastive_loss(
-                encoder_last_hidden_state,
-                self.encoder(notes_spectrogram).last_hidden_state,
+                encoder_out.last_hidden_state, encoder_out2.last_hidden_state
             )
             decoder_outputs["ct_loss"] = ct_loss
 
@@ -110,19 +117,22 @@ class EncoderDecoderTransformer(nn.Module):
     def generate(self, inputs: ModelInputs, **kwargs) -> torch.Tensor:
         encoder_inputs = self.spectrogram(inputs.input_waveform)
         b, _, h, w = encoder_inputs.shape
-        assert h == w == self.encoder_config.image_size
+        assert (
+            h == w == self.encoder_config.image_size
+        ), f"(n_mels, sequence length) = {(h,w)} and encoder image size ({self.encoder_config.image_siz}) mismatch"
 
-        encoder_last_hidden_state = self.encoder(encoder_inputs).last_hidden_state
+        encoder_out = self.encoder(encoder_inputs)
+        hidden_states = self.proj(encoder_out.last_hidden_state)
         if inputs.genre_id is None and inputs.difficulty_id is None:
-            encoder_hidden_states = encoder_last_hidden_state
+            hidden_states_conditioned = hidden_states
         else:
-            encoder_hidden_states = self.mel_conditioner(
-                encoder_last_hidden_state, inputs.genre_id, inputs.difficulty_id
+            hidden_states_conditioned = self.mel_conditioner(
+                hidden_states, inputs.genre_id, inputs.difficulty_id
             )
 
         decoder_outputs = self.decoder.generate(
             input_ids=torch.ones((b, 1)).long().to(self.decoder.device),
-            encoder_hidden_states=encoder_hidden_states,
+            encoder_hidden_states=hidden_states_conditioned,
             **kwargs,
         )
         return decoder_outputs
@@ -131,6 +141,7 @@ class EncoderDecoderTransformer(nn.Module):
 class Music2Midi(pl.LightningModule):
     def __init__(self, config_path: str):
         super().__init__()
+        self.config = OmegaConf.load(config_path)
         self.model = EncoderDecoderTransformer(config_path)
 
     def setup(self, stage=None):
@@ -146,21 +157,23 @@ class Music2Midi(pl.LightningModule):
     def training_step(self, inputs: ModelInputs, batch_idx):
         outputs = self.model(inputs)
         wandb_logs = {"epoch": self.current_epoch}
+
         loss = outputs.loss
-        self.log("ce_loss", loss, prog_bar=True)
+        self.log("ce_loss_train", loss, prog_bar=True)
         wandb_logs["ce_loss_train"] = loss
 
         if "ct_loss" in outputs.keys():
             ct_loss = outputs.ct_loss
             self.log("ct_loss_train", ct_loss, prog_bar=True)
             wandb_logs["ct_loss_train"] = ct_loss
-            loss = loss + ct_loss
+
         wandb.log(wandb_logs)
-        return loss
+        return loss + ct_loss
 
     def validation_step(self, inputs: ModelInputs, batch_idx):
         outputs = self.model(inputs)
         wandb_logs = {"epoch": self.current_epoch}
+
         loss = outputs.loss
         self.log("ce_loss_val", loss, prog_bar=True)
         wandb_logs["ce_loss_val"] = loss
@@ -169,9 +182,9 @@ class Music2Midi(pl.LightningModule):
             ct_loss = outputs.ct_loss
             self.log("ct_loss_val", ct_loss, prog_bar=True)
             wandb_logs["ct_loss_val"] = ct_loss
-            loss = loss + ct_loss
+
         wandb.log(wandb_logs)
-        return outputs.loss
+        return loss + ct_loss
 
     @torch.no_grad()
     def generate(
@@ -194,28 +207,33 @@ class Music2Midi(pl.LightningModule):
             sr = self.config.dataset.sample_rate
         if audio_y is None:
             audio_y, sr = librosa.load(str(audio_path), sr=sr)
+        split_size = int(sr * self.config.dataset.segment_duration)
+        pad_length = np.ceil(len(audio_y) / split_size).astype(int) * split_size - len(
+            audio_y
+        )
+        audio_y = np.pad(audio_y, (0, pad_length), "constant")
         waveform = torch.from_numpy(audio_y).to(self.device)
-        duration_per_batch = self.config.dataset.segment_duration
-        input_split, tokens_list = self.sample_tokens(
+        split_duration = self.config.dataset.segment_duration
+        numpy_notes = self.sample_tokens(
             waveform,
-            sr,
-            duration_per_batch,
+            split_size,
+            split_duration=split_duration,
             genre_id=genre_id,
             difficulty_id=difficulty_id,
         )
-        if self.tokenizer.time_step:
-            numpy_notes = self.tokenizer.batch_decode(
-                tokens_list, duration_per_batch=duration_per_batch
-            )
-        else:
-            numpy_notes_split = Parallel(n_jobs=multiprocessing.cpu_count() // 2)(
-                delayed(self.output_dtw)(waveform.cpu().numpy(), tokens, sr)
-                for waveform, tokens in zip(input_split, tokens_list)
-            )
-            # add time offset
-            for i, _ in enumerate(numpy_notes_split):
-                numpy_notes_split[i][:, :2] += i * duration_per_batch
-            numpy_notes = np.concatenate(numpy_notes_split)
+        # if self.model.tokenizer.time_step:
+        #     numpy_notes = self.model.tokenizer.decode(
+        #         tokens_list, mode="sequential", split_duration=split_duration
+        #     )
+        # else:
+        #     numpy_notes_split = Parallel(n_jobs=multiprocessing.cpu_count() // 2)(
+        #         delayed(self.output_dtw)(waveform.cpu().numpy(), tokens, sr)
+        #         for waveform, tokens in zip(input_split, tokens_list)
+        #     )
+        #     # add time offset
+        #     for i, _ in enumerate(numpy_notes_split):
+        #         numpy_notes_split[i][:, :2] += i * split_duration
+        #     numpy_notes = np.concatenate(numpy_notes_split)
 
         midi_data = numpy_to_midi(numpy_notes)
 
@@ -249,40 +267,44 @@ class Music2Midi(pl.LightningModule):
     def sample_tokens(
         self,
         waveform: torch.Tensor,
-        sr: int,
-        duration_per_batch: float,
+        split_size: float,
+        split_duration: float,
         genre_id: int = 0,
         difficulty_id: int = 0,
-    ) -> tuple[tuple[torch.Tensor], list[np.ndarray]]:
+    ) -> np.array:
         """
         Return: input tuple[waveform split, list of tokens]
         """
-        input_split = torch.split(waveform, sr * duration_per_batch)
+        input_split = torch.split(waveform, split_size)
 
         tokens_list = []
         for batch in more_itertools.chunked(
             input_split, self.config.inference.batch_size
         ):
             batch_size = len(batch)
-            inputs_embeds = pad_sequence(batch, batch_first=True, padding_value=0)
+            input_wav = pad_sequence(batch, batch_first=True, padding_value=0)
             genre_ids = torch.zeros((batch_size, 1)).long() + genre_id
             difficulty_ids = torch.zeros((batch_size, 1)).long() + difficulty_id
 
-            inputs_embeds = inputs_embeds.to(self.device)
+            input_wav = input_wav.to(self.device)
             genre_ids = genre_ids.to(self.device)
             difficulty_ids = difficulty_ids.to(self.device)
 
-            inputs_embeds = self.spectrogram(inputs_embeds).transpose(-1, -2)
-            inputs_embeds = self.mel_conditioner(
-                inputs_embeds, genre_ids, difficulty_ids
+            model_inputs = ModelInputs(
+                input_waveform=input_wav,
+                genre_id=genre_ids,
+                difficulty_id=difficulty_ids,
             )
-            tokens = self.t5model.generate(
-                inputs_embeds=inputs_embeds,
-                max_length=256,
+            tokens = self.model.generate(
+                model_inputs,
+                max_length=1024,
             )
-            tokens_list += [*tokens.cpu().numpy()]
+            tokens_list += [*tokens]
 
-        return input_split, tokens_list
+        numpy_notes = self.model.tokenizer.decode(
+            tokens_list, mode="sequential", duration_per_batch=split_duration
+        )
+        return numpy_notes
 
     def output_dtw(
         self, waveform: np.ndarray, tokens: np.ndarray, sr: int
